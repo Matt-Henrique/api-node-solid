53575d53a04ae41173f4ab6725324f7288d1a6df
Mateus H. Tofanello <mateus.h.tofanello@avanade.com>
2020-08-04 23:59:13 -0300

    api setup

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,33 @@
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
+
+export class MailtrapMailProvider implements IMailProvider {
+  private transporter: Mail;
+  
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: 'f9c25a05f8683e',
+        pass: 'a2dc45876feaac'
+      }
+    });
+  }
+  
+  async sendMail(message: IMessage): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email, 
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email,
+      },
+      subject: message.subject,
+      html: message.body,
+    });
+  }
+}
